684f0dc6ea78ca9af26fa68152936106b58b7d14
Wilson Estécio Marciílio Júnior <wilson_jr@outlook.com>
2017-03-07 09:23:59 -0300

    Remove debug statements

--- src/br/com/methods/overlap/OverlapRemoval.java
+++ src/br/com/methods/overlap/OverlapRemoval.java
@@ -24,20 +24,13 @@ public interface OverlapRemoval {
     public Map<OverlapRect, OverlapRect> apply(ArrayList<OverlapRect> rects);
     
     default public Map<OverlapRect, OverlapRect> applyAndShowTime(ArrayList<OverlapRect> rects) {
-        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ENTRADA@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-        for( int i = 0; i < rects.size(); ++i ) {
-            System.out.println(">>> "+i+": "+rects.get(i).x+"; "+rects.get(i).y);
+        for( int i = 0; i < rects.size(); ++i ) 
             rects.get(i).setId(i);
-        } 
+        
         long startTime = System.currentTimeMillis();
         Map<OverlapRect, OverlapRect> returned = apply(rects);
         long endTime = System.currentTimeMillis();
         Logger.getLogger(OverlapRemoval.class.getName()).log(Level.INFO, "Execution time "+toString()+": {0}", ((endTime-startTime)/1000.0));
-        
-        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@SAIDA@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-        returned.entrySet().forEach((e) -> {
-            System.out.println(">>> "+e.getKey().getId()+": "+e.getValue().x+"; "+e.getValue().y);
-        });
         
         return returned;                
     }
