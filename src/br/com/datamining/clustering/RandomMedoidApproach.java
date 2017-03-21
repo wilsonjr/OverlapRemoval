7fbc94d0c56febed6c966379de58a6d2f97408b8
Wilson Estécio Marciílio Júnior <wilson_jr@outlook.com>
2017-03-21 13:41:22 -0300

    Add random approach initialization for the kmeans

---
+++ src/br/com/datamining/clustering/RandomMedoidApproach.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.datamining.clustering;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author Windows
+ */
+public class RandomMedoidApproach implements InitialMedoidApproach {
+
+    @Override
+    public Point2D.Double[] getInitialGuess(ArrayList<Point2D.Double> items, int k) {
+        Random rnd = new Random();
+        
+        ArrayList<Integer> selected = new ArrayList<>();
+        Point2D.Double[] medoids = new Point2D.Double[k];
+        int count = 0;
+        
+        while( count < k ) {
+            int i = rnd.nextInt(items.size());
+            if( !selected.contains(i) ) 
+                medoids[count++] = items.get(i);
+        }
+        
+        return medoids;
+    }
+    
+}
