25fa8490dbe12fd831527bc0b9ec628a3d2e45b9
wilson_jr <wilson_jr@outlook.com>
2017-06-01 10:56:01 -0300

    Retrieve medoid from each node

--- src/br/com/representative/metric/SlimTreeNode.java
+++ src/br/com/representative/metric/SlimTreeNode.java
@@ -6,6 +6,7 @@
 
 package br.com.representative.metric;
 
+import br.com.methods.utils.Util;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,18 +16,20 @@ import java.util.List;
  * @author wilson
  */
 public class SlimTreeNode implements Comparable<SlimTreeNode> {
-    public List<Point2D.Double> points;
+    private List<Instance> points;
     
     public SlimTreeNode(List<Point2D.Double> points) {
-        this.points = points;
+        this.points = new ArrayList<>();
+        for( int i = 0; i < points.size(); ++i )
+            this.points.add(new Instance(points.get(i), i));
     }
     
     public SlimTreeNode() {
         points = new ArrayList<>();
     }
     
-    public void add(Point2D.Double p) {
-        points.add(p);
+    public void add(Point2D.Double p, int index) {
+        points.add(new Instance(p, index));
     }
     
     public int size() {
@@ -39,6 +42,45 @@ public class SlimTreeNode implements Comparable<SlimTreeNode> {
     }
 
     public Point2D.Double get(int i) {
-        return points.get(i);
+        return points.get(i).p;
+    }
+    
+    public int index(int i) {
+        return points.get(i).i;
+    }
+
+    public int medoid() {
+        
+        Point2D.Double centroid = new Point2D.Double(0, 0);
+        for( int i = 0; i < points.size(); ++i ) {
+            centroid.x += points.get(i).p.x;
+            centroid.y += points.get(i).p.y;
+        }
+        
+        centroid.x /= points.size();
+        centroid.y /= points.size();
+        
+        int indexCentroid = -1;
+        double minDist = Double.MAX_VALUE;
+        for( int i = 0; i < points.size(); ++i ) {
+            double d = Util.euclideanDistance(centroid.x, centroid.y, points.get(i).p.x, points.get(i).p.y);
+            if( d < minDist ) {
+                minDist = d;
+                indexCentroid = i;
+            }
+        }        
+        
+        return points.get(indexCentroid).i;
+    }
+    
+    
+    private class Instance {
+        private Point2D.Double p;
+        private int i;
+        
+        public Instance(Point2D.Double p, int index) {
+            this.p = p;
+            this.i = index;
+        }
     }
 }
