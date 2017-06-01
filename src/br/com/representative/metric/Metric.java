ad2290f4404668428e4f7b9f5440d74afc39f537
Wilson Estécio Marciílio Júnior <wilson_jr@outlook.com>
2017-06-01 15:13:52 -0300

    Add Metric class to refactore other metric approaches

---
+++ src/br/com/representative/metric/Metric.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.representative.metric;
+
+import br.com.representative.RepresentativeFinder;
+import java.awt.geom.Point2D;
+import java.util.List;
+
+/**
+ *
+ * @author Windows
+ */
+public abstract class Metric extends RepresentativeFinder {
+    protected List<Point2D.Double> finalItems;
+    protected List<Point2D.Double> items;
+    
+    public Metric(List<Point2D.Double> items) {
+        this.finalItems = items;
+        this.items = items;
+    }
+    
+    @Override
+    public void filterData(int[] indexes) {
+        items.clear();
+        
+        for( Integer i: indexes )
+            items.add(finalItems.get(i));   
+    }
+    
+}
