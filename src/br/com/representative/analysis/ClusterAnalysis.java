dee22d92eb464c7d4c173fde85a8437ac28df33a
wilson_jr <wilson_jr@outlook.com>
2017-09-06 13:11:23 -0300

    Add other analysis metrics

---
+++ src/br/com/representative/analysis/ClusterAnalysis.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.representative.analysis;
+
+import br.com.methods.utils.Util;
+import java.awt.geom.Point2D;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author wilson
+ */
+public class ClusterAnalysis implements RepresentativeAnalysis {
+
+    private int[] representative;
+    private Point2D.Double[] points;
+    
+    public ClusterAnalysis(int[] representative, Point2D.Double[] points) {
+        this.representative = representative;
+        this.points = points;
+    }
+    
+    
+    @Override
+    public double init() {
+        
+        double variance = 0.0, mean = (double)points.length/(double)representative.length;
+        
+        Map<Integer, List<Integer>> indexes = Util.createIndex(representative, points);
+        
+        variance = indexes.entrySet().stream().map(
+                (v) -> (v.getValue().size()-mean)*(v.getValue().size()-mean)
+        ).reduce(
+                variance, 
+                (accumulator, _item) -> accumulator + _item        
+        );
+        
+        variance /= representative.length;
+        
+        
+        return variance;
+    }
+    
+}
